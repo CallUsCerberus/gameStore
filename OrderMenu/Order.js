7333b1241f3ccdd57a5baa04d76c971a679178c6
CallUsCerberus <50325355+CallUsCerberus@users.noreply.github.com>
2019-05-07 03:13:53 +0300

    Added Order Menu and Local Storage Operations, so the order does not disappear

--- OrderMenu/Order.js
+++ OrderMenu/Order.js
@@ -1,8 +1,104 @@
+var pricesArray = [1200, 1500, 900, 1050, 650, 850, 1000, 1300];
+var amountArray = [0, 0, 0, 0, 0, 0, 0, 0];
+var namesArray = ["Devil May Cry 5(ПК)", "Devil May Cry 5(Консоль)", "Sekiro: Shadows Die Twice(ПК)",
+   "Sekiro: Shadows Die Twice(Консоль)", "Assassin`s Creed : Odyssey(ПК)", "Assassin`s Creed : Odyssey(Консоль)",
+   "Mortal Combat 11(ПК)", "Mortal Combat 11(Консоль)"
+];
+var srcArray = [$(".img-1").html(), $(".img-2").html(), $(".img-3").html(), $(".img-4").html()];
 $(function () {
+   var price = localStorage.getItem("sumPrice");
+   alert(price);
+   if(localStorage.length!=0){
+      if(Number(localStorage.getItem(namesArray[0]))!=0){
+         var img = $(".img-1").html();
+         var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
+         $(cloned).css('position', 'relative');
+         $(cloned).find(".rname").text(namesArray[0]);
+         $(cloned).find(".rprice").text(pricesArray[0]);
+         $(cloned).find(".inp").val(localStorage.getItem(namesArray[0]));
+         $(cloned).find(".side-img").html(img);
+         cloned.appendTo(".bin-2");
+      }
+      if(Number(localStorage.getItem(namesArray[1]))!=0){
+         var img = $(".img-1").html();
+         var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
+         $(cloned).css('position', 'relative');
+         $(cloned).find(".rname").text(namesArray[1]);
+         $(cloned).find(".rprice").text(pricesArray[1]);
+         $(cloned).find(".inp").val(localStorage.getItem(namesArray[1]));
+         $(cloned).find(".side-img").html(img);
+         cloned.appendTo(".bin-2");
+      }
+      if(Number(localStorage.getItem(namesArray[2]))!=0){
+         var img = $(".img-2").html();
+         var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
+         $(cloned).css('position', 'relative');
+         $(cloned).find(".rname").text(namesArray[2]);
+         $(cloned).find(".rprice").text(pricesArray[2]);
+         $(cloned).find(".inp").val(localStorage.getItem(namesArray[2]));
+         $(cloned).find(".side-img").html(img);
+         cloned.appendTo(".bin-2");
+      }
+      if(Number(localStorage.getItem(namesArray[3]))!=0){
+         var img = $(".img-2").html();
+         var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
+         $(cloned).css('position', 'relative');
+         $(cloned).find(".rname").text(namesArray[3]);
+         $(cloned).find(".rprice").text(pricesArray[3]);
+         $(cloned).find(".inp").val(localStorage.getItem(namesArray[3]));
+         $(cloned).find(".side-img").html(img);
+         cloned.appendTo(".bin-2");
+      }
+      if(Number(localStorage.getItem(namesArray[4]))!=0){
+         var img = $(".img-3").html();
+         var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
+         $(cloned).css('position', 'relative');
+         $(cloned).find(".rname").text(namesArray[4]);
+         $(cloned).find(".rprice").text(pricesArray[4]);
+         $(cloned).find(".inp").val(localStorage.getItem(namesArray[4]));
+         $(cloned).find(".side-img").html(img);
+         cloned.appendTo(".bin-2");
+      }
+      if(Number(localStorage.getItem(namesArray[5]))!=0){
+         var img = $(".img-3").html();
+         var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
+         $(cloned).css('position', 'relative');
+         $(cloned).find(".rname").text(namesArray[5]);
+         $(cloned).find(".rprice").text(pricesArray[5]);
+         $(cloned).find(".inp").val(localStorage.getItem(namesArray[5]));
+         $(cloned).find(".side-img").html(img);
+         cloned.appendTo(".bin-2");
+      }
+      if(Number(localStorage.getItem(namesArray[6]))!=0){
+         var img = $(".img-4").html();
+         var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
+         $(cloned).css('position', 'relative');
+         $(cloned).find(".rname").text(namesArray[6]);
+         $(cloned).find(".rprice").text(pricesArray[6]);
+         $(cloned).find(".inp").val(localStorage.getItem(namesArray[6]));
+         $(cloned).find(".side-img").html(img);
+         cloned.appendTo(".bin-2");
+      }
+      if(Number(localStorage.getItem(namesArray[7]))!=0){
+         var img = $(".img-4").html();
+         var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
+         $(cloned).css('position', 'relative');
+         $(cloned).find(".rname").text(namesArray[7]);
+         $(cloned).find(".rprice").text(pricesArray[7]);
+         $(cloned).find(".inp").val(localStorage.getItem(namesArray[7]));
+         $(cloned).find(".side-img").html(img);
+         cloned.appendTo(".bin-2");
+      }
+   }
+
+
    var todaysDate=new Date();
    var currentDate=new Date();
    startTime();
    createCalendar('calendar2', new Date());
+
+   $("#name").hide();
+   $("#name2").hide();
 
    $(".buttonLeft").click(function () {
       currentDate.setMonth(currentDate.getMonth()+1);
@@ -13,12 +109,44 @@ $(function () {
       createCalendar('calendar2', currentDate);
    });
 
+   $("#inputName").keyup(function () {
+      var text = $("#inputName").val();
+      alert(text);
+      if (text.charAt(text.length-1)!=" ") {
+         if (!(text.charAt(text.length - 1) >= 'А' && text.charAt(text.length - 1) <= 'я')) {
+            $("#inputName").css("backgroundColor", "lightcoral");
+            $("#inputName").val(text.substring(0, text.length - 1)) ;
+            $("#name").show();
 
+         } else {
+            $("#inputName").css("backgroundColor", "lightgreen");
+            $("#name").hide();
+         }
+      }
 
+   });
+   $("#inputName2").keyup(function () {
+      var text = $("#inputName2").val();
+      alert(text);
+      if (text.charAt(text.length-1)!=" ") {
+         if (!(text.charAt(text.length - 1) >= 'А' && text.charAt(text.length - 1) <= 'я')) {
+            $("#inputName2").css("backgroundColor", "lightcoral");
+            $("#inputName2").val(text.substring(0, text.length - 1)) ;
+            $("#name2").show();
+
+         } else {
+            $("#inputName2").css("backgroundColor", "lightgreen");
+            $("#name2").hide();
+         }
+      }
+
+   });
 
 
 
    ///////////////////////////////////////////////////////////////////
+
+
    function startTime()
    {
       var tm=new Date();
--- anime.js
+++ anime.js
@@ -1,30 +1,143 @@
+
+var pricesArray = [1200, 1500, 900, 1050, 650, 850, 1000, 1300];
+var amountArray = [0, 0, 0, 0, 0, 0, 0, 0];
+var namesArray = ["Devil May Cry 5(ПК)", "Devil May Cry 5(Консоль)", "Sekiro: Shadows Die Twice(ПК)",
+    "Sekiro: Shadows Die Twice(Консоль)", "Assassin`s Creed : Odyssey(ПК)", "Assassin`s Creed : Odyssey(Консоль)",
+    "Mortal Combat 11(ПК)", "Mortal Combat 11(Консоль)"
+];
+var srcArray = [$(".img-1").html(), $(".img-2").html(), $(".img-3").html(), $(".img-4").html()];
+if(localStorage.length==0) {
+    alert("zer0")
+    localStorage.setItem("Devil May Cry 5(ПК)", 0);
+    localStorage.setItem("Devil May Cry 5(Консоль)", 0);
+    localStorage.setItem("Sekiro: Shadows Die Twice(ПК)", 0);
+    localStorage.setItem("Sekiro: Shadows Die Twice(Консоль)", 0);
+    localStorage.setItem("Assassin`s Creed : Odyssey(ПК)", 0);
+    localStorage.setItem("Assassin`s Creed : Odyssey(Консоль)", 0);
+    localStorage.setItem("Mortal Combat 11(ПК)", 0);
+    localStorage.setItem("Mortal Combat 11(Консоль)", 0);
+}
 $(document).ready(function(){
+
+    if(localStorage.length!=0){
+        if(Number(localStorage.getItem(namesArray[0]))!=0){
+            var img = $(".img-1").html();
+            var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
+            $(cloned).css('position', 'relative');
+            $(cloned).find(".rname").text(namesArray[0]);
+            $(cloned).find(".rprice").text(pricesArray[0]);
+            $(cloned).find(".inp").val(localStorage.getItem(namesArray[0]));
+            $(cloned).find(".side-img").html(img);
+            cloned.appendTo(".col-md-2");
+        }
+        if(Number(localStorage.getItem(namesArray[1]))!=0){
+            var img = $(".img-1").html();
+            var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
+            $(cloned).css('position', 'relative');
+            $(cloned).find(".rname").text(namesArray[1]);
+            $(cloned).find(".rprice").text(pricesArray[1]);
+            $(cloned).find(".inp").val(localStorage.getItem(namesArray[1]));
+            $(cloned).find(".side-img").html(img);
+            cloned.appendTo(".col-md-2");
+        }
+        if(Number(localStorage.getItem(namesArray[2]))!=0){
+            var img = $(".img-2").html();
+            var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
+            $(cloned).css('position', 'relative');
+            $(cloned).find(".rname").text(namesArray[2]);
+            $(cloned).find(".rprice").text(pricesArray[2]);
+            $(cloned).find(".inp").val(localStorage.getItem(namesArray[2]));
+            $(cloned).find(".side-img").html(img);
+            cloned.appendTo(".col-md-2");
+        }
+        if(Number(localStorage.getItem(namesArray[3]))!=0){
+            var img = $(".img-2").html();
+            var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
+            $(cloned).css('position', 'relative');
+            $(cloned).find(".rname").text(namesArray[3]);
+            $(cloned).find(".rprice").text(pricesArray[3]);
+            $(cloned).find(".inp").val(localStorage.getItem(namesArray[3]));
+            $(cloned).find(".side-img").html(img);
+            cloned.appendTo(".col-md-2");
+        }
+        if(Number(localStorage.getItem(namesArray[4]))!=0){
+            var img = $(".img-3").html();
+            var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
+            $(cloned).css('position', 'relative');
+            $(cloned).find(".rname").text(namesArray[4]);
+            $(cloned).find(".rprice").text(pricesArray[4]);
+            $(cloned).find(".inp").val(localStorage.getItem(namesArray[4]));
+            $(cloned).find(".side-img").html(img);
+            cloned.appendTo(".col-md-2");
+        }
+        if(Number(localStorage.getItem(namesArray[5]))!=0){
+            var img = $(".img-3").html();
+            var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
+            $(cloned).css('position', 'relative');
+            $(cloned).find(".rname").text(namesArray[5]);
+            $(cloned).find(".rprice").text(pricesArray[5]);
+            $(cloned).find(".inp").val(localStorage.getItem(namesArray[5]));
+            $(cloned).find(".side-img").html(img);
+            cloned.appendTo(".col-md-2");
+        }
+        if(Number(localStorage.getItem(namesArray[6]))!=0){
+            var img = $(".img-4").html();
+            var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
+            $(cloned).css('position', 'relative');
+            $(cloned).find(".rname").text(namesArray[6]);
+            $(cloned).find(".rprice").text(pricesArray[6]);
+            $(cloned).find(".inp").val(localStorage.getItem(namesArray[6]));
+            $(cloned).find(".side-img").html(img);
+            cloned.appendTo(".col-md-2");
+        }
+        if(Number(localStorage.getItem(namesArray[7]))!=0){
+            var img = $(".img-4").html();
+            var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
+            $(cloned).css('position', 'relative');
+            $(cloned).find(".rname").text(namesArray[7]);
+            $(cloned).find(".rprice").text(pricesArray[7]);
+            $(cloned).find(".inp").val(localStorage.getItem(namesArray[7]));
+            $(cloned).find(".side-img").html(img);
+            cloned.appendTo(".col-md-2");
+        }
+    }
+
     $(".news").click(function(){
-        alert("fuck");
         window.location.href='http://localhost:63342/GamesStore/OrderMenu/Order.html?_ijt=dsd1p1gpmdg63i61s557g10s59';
+        var price = Number($("#sumPrice").text());
+        localStorage.setItem("sumPrice", price);
+        alert(price);
     });
 
     $(".btn-pc").click(function(){
         var name = $(this).parent().parent().find(".name").text();
         var price = Number($(this).parent().parent().find(".PCPrice").text());
         name += "(ПК)";
-        var cloneImg = $(this).parent().parent().parent().find(".main-img").html();
-        var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
-        $(cloned).css('position', 'relative');
-        $(cloned).find(".rname").text(name);
-        $(cloned).find(".rprice").text(price);
-        $(cloned).find(".side-img").html(cloneImg);
-        $(cloned).appendTo(".col-md-2");
-        var prev = Number($("#sumPrice").text());
-        prev += price;
-        $("#sumPrice").text(prev);
-        alert(name);
+
+            var currentAmount = Number(localStorage.getItem(name));
+            currentAmount++;
+            localStorage.setItem(name, currentAmount);
+            var cloneImg = $(this).parent().parent().parent().find(".main-img").html();
+            var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
+            $(cloned).css('position', 'relative');
+            $(cloned).find(".rname").text(name);
+            $(cloned).find(".rprice").text(price);
+            $(cloned).find(".side-img").html(cloneImg);
+            $(cloned).appendTo(".col-md-2");
+            var prev = Number($("#sumPrice").text());
+            prev += price;
+            $("#sumPrice").text(prev);
+            alert(localStorage.getItem(name));
+
     });
 
     $(".btn-c").click(function(){
         var name = $(this).parent().parent().find(".name").text();
         var price = Number($(this).parent().parent().find(".CPrice").text());
         name += "(Консоль)";
+        var currentAmount = Number(localStorage.getItem(name));
+        currentAmount++;
+        localStorage.setItem(name, currentAmount);
         var cloneImg = $(this).parent().parent().parent().find(".main-img").html();
         var cloned = $("#toBeCloned").clone(true).css('visibility', 'visible');
         $(cloned).css('position', 'relative');
@@ -32,10 +145,11 @@ $(document).ready(function(){
         $(cloned).find(".rprice").text(price);
         $(cloned).find(".side-img").html(cloneImg);
         $(cloned).appendTo(".col-md-2");
+        $(cloned).appendTo(".bin-2");
         var prev = Number($("#sumPrice").text());
         prev += price;
         $("#sumPrice").text(prev);
-        alert(name);
+        alert(localStorage.getItem(name));
     });
 
     $(".plus").click(function () {
